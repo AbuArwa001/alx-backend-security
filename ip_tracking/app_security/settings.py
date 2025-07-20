1a536df8908054c2595f5577590527452315dd09
khalfan <AbuuArwa001>
2025-07-20 05:19:41 +0300

    feat: add rate limiting to login view and update settings

--- ip_tracking/app_security/settings.py
+++ ip_tracking/app_security/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Rate limiting settings
+RATELIMIT_ENABLE = True
+RATELIMIT_VIEW = 'ip_tracking.views.CustomLoginView'
--- ip_tracking/ip_tracking/views.py
+++ ip_tracking/ip_tracking/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django_ratelimit.decorators import ratelimit
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    @ratelimit(key='ip', rate='5/m', method='POST', block=True)
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
